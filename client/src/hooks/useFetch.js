ca2c974b434cc0a6f05b6628922731e147d0eec8
OliveLeb <lebel.olivier@wanadoo.fr>
2020-10-31 05:03:49 +0100

    delete console.log in useFetch

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -2,30 +2,42 @@ import { useEffect, useRef } from 'react'
 
 export const useFetch = (axiosService, fetch, loading, fetchError, isLogged) => {
 
-    const cache = useRef({});
+    //const cache = useRef(null);
+
     useEffect(() => {
+        let cancelRequest = false;
+        if(!axiosService) return ;
 
         const fetchData = async () => {
+            
             loading();
 
-            if(cache.current[axiosService]) {
+           /* if(cache.current[axiosService]) {
                 const data = cache.current[axiosService];
                 fetch(data);
                 console.log(data)
+                console.log(cache.current)
             }
-            else {
+            else {*/
                 try {
                     const result = await axiosService.get();
-                    cache.current[axiosService] = result;
+                    //cache.current[axiosService] = result;
+                    //console.log(cache.current[axiosService]);
+                    if (cancelRequest) return;
                     fetch(result);
                 }
                 catch(error) {
+                    if (cancelRequest) return;
                     fetchError();
                 }
-            }
+            //}
         }
 
         isLogged && fetchData();
+
+        return function cleanup() {
+			cancelRequest = true;
+		};
 
     },[axiosService,isLogged])
 
