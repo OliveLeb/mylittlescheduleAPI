a870630219237417017caf120266bc29f954d8b9
OliveLeb <lebel.olivier@wanadoo.fr>
2020-10-31 18:13:03 +0100

    adding expiresIn 2h token

--- utils/token.js
+++ utils/token.js
@@ -7,7 +7,7 @@ dotenv.config();
 module.exports = (user) => {
     
     const credentials = {id:user.id,admin:user.is_admin};
-    const token = jwt.sign(credentials,process.env.SECRET_TOKEN);
+    const token = jwt.sign(credentials,process.env.SECRET_TOKEN, { expiresIn: '2h'});
     return token;
 }
 
